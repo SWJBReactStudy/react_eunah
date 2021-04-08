ed0411df56b4556cbab46ab4b6466a2ee7284629
jd053303 <jd053303@gmail.com>
2021-04-08 10:01:23 +0900

    연습
    함수형

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import fcat from './img/ha.jpg';
 import scat from './img/he.jpg';
@@ -6,44 +7,44 @@ import tcat from './img/ho.jpg';
 
 function App() {
 
-  const cats = [
-    {
-      head : {name: 'MU YA-ONG'},
-      img : [
-        {id : 1, name: 'hodong', catimg : fcat},
-        {id : 2, name: 'hwak', catimg : scat},
-        {id : 3, name: 'myo', catimg : tcat}
-      ]
-    }
-  ];
-
+  const [cats, set_id] = useState({
+    _id : 0,
+    head :  'MU YA-ONG',
+    img : [
+      { name: 'hodong', catimg : fcat},
+      { name: 'hwak', catimg : scat},
+      { name: 'myo', catimg : tcat}
+    ]
+  });
+  
   const setimg = () =>{
-    if(_id>2) setcatimg(count => count+1);
-    else setcatimg(0);
+    count++;
+    if(count==3) count=0;
+    set_id({
+      _id : count 
+    });
   }
 
-  const [_id, ] = useState(0);
-
-  var count = _id;
-  const _cat = cats.img[cat._id].cating;
-  const _name = cats.img[cat._id].name;
+  const count = cats._id;
+  const _catimg = cats.img[cats._id].catimg;
+  const _name = cats.img[cats._id].name;
 
   return (
-    <div class="wrap">
-      <div class="mooyaho">
+    <div className="wrap">
+      <div className="mooyaho">
         <header>
-          <div class="logoboard">
-            <img src={fcat} class="logo" alt="teat1234"></img>
+          <div className="logoboard">
+            <img src={_catimg} className="logo" alt="teat1234"></img>
           </div>
-          <h1 class="my"><a href="/">{this.state.head.name}</a></h1>
+          <h1 className="my"><a href="/">{cats.head}</a></h1>
         </header> 
-        <div class="imgcat">
-          <a href="/"><img src={_cat} alt="cat"></img></a>
-          <a href="/"><img src={_cat} alt="cat"></img></a>
-          <a href="/"><img src={_cat} alt="cat"></img></a>
+        <div className="imgcat">
+          <a href="/"><img src={_catimg} alt="cat"></img></a>
+          <a href="/"><img src={_catimg} alt="cat"></img></a>
+          <a href="/"><img src={_catimg} alt="cat"></img></a>
         </div>
-        <div class="gotohouse">
-            <button class="Btn" onClick={setimg}>{_name}</button>
+        <div className="gotohouse">
+            <button className="Btn" onClick={setimg}>{_name}</button>
           </div>
       </div>
     </div>
